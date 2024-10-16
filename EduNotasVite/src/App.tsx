82db8b2343e673bf30b34177feddbdfa7c64a021
jefer <cuetiajefer1103@gmail.com>
2024-10-16 13:26:04 -0500

    avance base a implementar

---
+++ EduNotasVite/src/App.tsx
@@ -0,0 +1,14 @@
+import Plantilla from './components/base/plantilla'; 
+
+function App () {
+  return (
+    <>
+      <Plantilla>
+        <h1>Bienvenido a la Aplicación</h1>
+        <p>Este es el contenido de la plantilla.</p>
+      </Plantilla>
+    </>
+  )
+}
+export default App;
+          
