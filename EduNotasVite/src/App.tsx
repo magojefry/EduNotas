8a45be7b86e730b3fdea74317a7a9f59644d5019
Carlos Quiñones <cquinones@unimayor.edu.co>
2024-10-22 18:06:57 -0500

    18/10/2024
    primer_ventana

--- EduNotasVite/src/App.tsx
+++ EduNotasVite/src/App.tsx
@@ -1,12 +1,13 @@
 import Epilogo from "./components/registro_notas_grupo/epilogo"
 import Header from "./components/registro_notas_grupo/header"
 import Svg_registro_notas_grupo from "./components/registro_notas_grupo/svg_notas_grupo"
+import MatricularMateria from './components/registrar_materia/matricularMateria';
 import Tabla_registro_notas_grupo from "./components/registro_notas_grupo/tabla_registro_notas grupo"
 import Tabla_registro_notas from "./components/registro_notas/tabla_registro_notas"
 import Login from "./components/login/login";
 import Menu_registro_docente from "./components/menu_regist/menu_registro_docente"
 
-function App () {
+function App() {
   return (
     <>
       {/* Se comenta con el fin de a futuro crear la plantilla para espaciar cada proyecto
@@ -25,11 +26,12 @@ function App () {
       {/*<Login/>
         <Epilogo/>
         */}
-      
+
       <Header></Header>
-        <Menu_registro_docente/>
-      <Epilogo/>
-       
+
+      <MatricularMateria />
+      <Epilogo />
+
     </>
   )
 }
