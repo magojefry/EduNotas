8fa178f4ac8b4922141f5a3983deceae9a54dfb7
jefer <cuetiajefer1103@gmail.com>
2024-11-02 19:20:56 -0500

    se implemento modelos para base de datos y django administrador

--- EduNotasDjango/apps/core/models.py
+++ EduNotasDjango/apps/core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+class Usuario(AbstractUser):
+    ROLES = [
+        ('estudiante', 'Estudiante'),
+        ('profesor', 'Profesor'),
+        ('administrador', 'Administrador'),
+    ]
+    persona = models.OneToOneField(Persona, on_delete=models.CASCADE, null=True, blank=True)
+    rol = models.CharField(max_length=50, choices=ROLES)
+    
+    def save(self, *args, **kwargs):
+        if self.persona:
+            self.first_name = self.persona.nombre
+            self.last_name = self.persona.apellido
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20)
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Asignacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    materias = models.ManyToManyField(Materia, related_name='asignaciones')  # Cambiado a ManyToManyField
+    año = models.PositiveIntegerField()
+
+class Evaluacion(models.Model):
+    asignacion = models.ForeignKey(Asignacion, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=50)
+    fecha = models.DateField()
+    valor = models.FloatField(null=True)
+
+
--- EduNotasDjango/settings.py
+++ EduNotasDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #nuevas apps a implementar
+    'EduNotasDjango.apps.core'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "core.Usuario"
